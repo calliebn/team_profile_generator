80e921f9ca84b54fd5e1380f334bcf4b288ccee5
Callie Nipper <callie.nipper@gmail.com>
2021-03-28 10:15:11 -0700

    Started adding questins to index.js file. Started template for html file. Added test files for each type of team member.

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
+const fs = require ('fs')
+const inquirer = require ('inquirer')
 
+const questions = [
+    {
+        type: 'input',
+        message: 'What is the managers name?',
+        name: 'managerName'
+    },
+
+    {
+        type: 'input',
+        message: 'What is the managers employee ID?',
+        name: 'managerID'
+    },
+
+    {
+        type: 'input',
+        message: 'What is the managers email address?',
+        name: 'managerEmail'
+    },
+
+    {
+        type: 'input',
+        message: 'What is the managers office number?',
+        name: 'managerOffice'
+    },
+
+    {
+        type: 'confirm',
+        message: 'Are you done building your team?',
+        
+    },
+
+    {
+    type: 'list',
+    message: 'What would you like to do next?',
+    name: 'breakOne',
+    choices: ['Add an engineer', 'Add an intern', 'Finish building team'],
+
+
+    }
+]
